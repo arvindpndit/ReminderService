21eb5f9c7cb1e361b83c57df88653a141f79327c
arvindpndit <arvindpandit9808@gmail.com>
2023-09-08 10:07:20 +0530

    added rabbit mq config

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
+const { createChannel } = require("./utils/messageQueue");
 //const { sendBasicEmail } = require("./services/email-service");
 const jobs = require("./utils/job");
 const TicketController = require("./controllers/ticket-controller");
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  const channel = await createChannel();
 
   app.post("/api/v1/tickets", TicketController.create);
 
